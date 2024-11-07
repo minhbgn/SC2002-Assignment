50a1a510b0ad35fb1474231772928c387a975182
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-07 23:21:19 +0800

    fix: Wrong type for data in read() and write()

--- src/hms/utils/CSVFileHandler.java
+++ src/hms/utils/CSVFileHandler.java
@@ -1,13 +1,16 @@
 package hms.utils;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class CSVFileHandler {
-    public HashMap<String,String> read(String filePath) {
+    public ArrayList<HashMap<String, String>> read(String filePath) {
         throw new UnsupportedOperationException("Not implemented");
     }
 
-    public void write(String filePath, HashMap<String,String> data) {
+    public void write(String filePath, List<HashMap<String,String>> data) {
         throw new UnsupportedOperationException("Not implemented");
     }
 }
