b32b65e9f03d2856fb800a00e66b0be12af16792
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-14 02:31:37 +0800

    Reimplement selector

--- src/hms/ui/PaginatedListSelector.java
+++ src/hms/ui/PaginatedListSelector.java
@@ -1,14 +1,29 @@
 package hms.ui;
 
+import java.util.function.Consumer;
+
 public class PaginatedListSelector<T> extends PaginatedListViewer<T>{
-    public PaginatedListSelector(String title, T[] items){
+    private final Consumer<T> onSelect;
+
+    public PaginatedListSelector(String title, T[] items, Consumer<T> onSelect){
         super(title, items);
+
+        this.onSelect = onSelect;
     }
 
-    public T select(int index){
-        if(0 <= index && index < paginatedList.getLength())
-            return paginatedList.get(index);
-        
-        return null;
+    @Override
+    public void executeOption(String key){
+        if (hasOption(key)){
+            options.get(key).execute();
+        }
+
+        try{
+            int index = Integer.parseInt(key) - 1;
+            if (0 <= index && index < paginatedList.getLength()){
+                onSelect.accept(paginatedList.getItem(index));
+            }
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("Invalid option");
+        }
     }
 }
