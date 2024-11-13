a2dd26c2e2940b2c7a6da16e4c42e0e0ee1e759a
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-14 02:29:52 +0800

    refactor: Extract methods to simplify code

--- src/hms/ui/MenuNavigator.java
+++ src/hms/ui/MenuNavigator.java
@@ -1,60 +1,48 @@
 package hms.ui;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Stack;
 
 public class MenuNavigator extends AbstractMenu {
     private final Stack<AbstractMenu> history = new Stack<>();
     private final HashMap<String, UserOption> navigatorOptions = new HashMap<>();
 
-    public MenuNavigator(String title) {
-        this.title = title;
-        this.options = new LinkedHashMap<>();
+    public MenuNavigator() {
+        this.options = new HashMap<>();
 
-        navigatorOptions.put("b", new UserOption("Back", () -> {
-            if (history.size() > 1) {
-                goBack();
-            }
-            else{
-                System.out.println("Cannot go back");
-            }
-        }));
+        navigatorOptions.put("b", new UserOption("Back", () -> goBack()));
 
         options.putAll(navigatorOptions);
     }
 
     public void addMenu(AbstractMenu menu) {
         history.push(menu);
-        
-        options.clear();
-        options.putAll(menu.options);
-        if (history.size() > 1) {
-            options.putAll(navigatorOptions);
-        }
     }
 
     private void goBack() {
         if (history.size() > 1) {
             history.pop();
-
-            options.clear();
-            options.putAll(history.peek().options);
-
-            if (history.size() > 1) {
-                options.putAll(navigatorOptions);
-            }
         }
-        else{
-            throw new IllegalStateException("Cannot go back from root menu");
+    }
+
+    private void updateOptions() {
+        options.clear();
+        options.putAll(history.peek().options);
+
+        if (history.size() > 1) {
+            options.putAll(navigatorOptions);
         }
     }
 
     @Override
-    public void display() {
-        System.out.println(title);
+    public void display(boolean showOptions) {
+        history.peek().display(false);
 
-        System.out.println("Options:");
-        options.forEach((key, value) -> System.out.println(key + " - " + value.getText()));
+        if (showOptions) {
+            updateOptions();
+
+            System.out.println("\nOptions:");
+            options.forEach((key, value) -> System.out.println(key + " - " + value.getText()));
+        }
     }
 }
