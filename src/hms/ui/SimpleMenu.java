037e00cfe1fa0c69a8d3e0834d2c7732484e73b7
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-14 21:04:09 +0800

    Add addOption() to SimpleMenu

--- src/hms/ui/SimpleMenu.java
+++ src/hms/ui/SimpleMenu.java
@@ -8,9 +8,17 @@ public class SimpleMenu extends AbstractMenu {
         this.title = title;
         this.options = new LinkedHashMap<>();
 
+        if (options == null){
+            return;
+        }
+        
         for (int i = 0; i < options.size(); i++){
             this.options.put(Integer.toString(i + 1), options.get(i));
         }
+    }
+
+    public void addOption(UserOption option){
+        options.put(Integer.toString(options.size() + 1), option);
     }
 
     @Override
@@ -18,7 +26,13 @@ public class SimpleMenu extends AbstractMenu {
         System.out.println(title);
 
         if (showOptions){
-            options.forEach((key, value) -> System.out.println(key + ". " + value.getText()));
+            options.forEach(
+                (key, value) -> System.out.println(
+                String.format(
+                    key.matches("\\d+") ? "%s. %s" : "%s - %s",
+                    key, value.getText()
+                )
+            ));
         }
     }
 }
