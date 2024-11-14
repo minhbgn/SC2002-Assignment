f96fbb86a36df2d59a0921e896a934f35984daad
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-15 03:29:46 +0800

    Handle ViewAppointment on doctor side

--- src/hms/system/service/ViewAppointmentsService.java
+++ src/hms/system/service/ViewAppointmentsService.java
@@ -6,6 +6,7 @@ import hms.common.SearchCriterion;
 import hms.manager.AppointmentManager;
 import hms.manager.ManagerContext;
 import hms.manager.PrescriptionManager;
+import hms.manager.UserManager;
 import hms.prescription.Prescription;
 import hms.ui.AbstractMenu;
 import hms.ui.MenuNavigator;
@@ -17,6 +18,7 @@ import hms.user.model.Admin;
 import hms.user.model.Doctor;
 import hms.user.model.Patient;
 import hms.user.model.User;
+import hms.user.repository.PatientRepository;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -30,6 +32,9 @@ public class ViewAppointmentsService implements IService {
     private final UserOption rescheduleAppointmentOption = new UserOption("Reschedule Appointment", this::handleRescheduleAppointment);
     private final UserOption cancelAppointmentOption = new UserOption("Cancel Appointment", this::handleCancelAppointment);
     private final UserOption viewRecordsOption = new UserOption("View Records", this::handleViewRecords);
+    private final UserOption resolveAppointmentOption = new UserOption("Accept Appointment", this::handleResolveAppointment);
+    private final UserOption completeAppointmentOption = new UserOption("Complete Appointment", this::handleCompleteAppointment);
+    private final UserOption viewPatientInfoOption = new UserOption("View Patient Info", this::handleViewPatientInfo);
 
     /** Variable for keeping track of the selected appointment */
     private Appointment selected;
@@ -87,6 +92,61 @@ public class ViewAppointmentsService implements IService {
         menuNav.addMenu(recordMenu);
     }
 
+    private void handleResolveAppointment() {
+        Doctor d = (Doctor) user;
+
+        boolean accepted = Prompt.getBooleanInput("Accept appointment? (y/n): ");
+
+        d.resolvePendingAppointment(selected.getId(), accepted);
+    }
+
+    private void handleCompleteAppointment() {
+        String notes = Prompt.getStringInput("Enter notes for the appointment: ");
+        notes = notes.replace("\n", " ");
+
+        String service = Prompt.getStringInput("Enter services provided: ");
+
+        PrescriptionManager prescriptionManager = ctx.getManager(PrescriptionManager.class);
+        ArrayList<String> prescriptionIds = new ArrayList<>();
+
+        while(Prompt.getBooleanInput("Add a prescription? (y/n): ")) {
+            String medicalName = Prompt.getStringInput("Enter the medicine name: ");
+            int quantity = Prompt.getIntInput("Enter the quantity: ");
+
+            if(quantity <= 0) {
+                System.out.println("Invalid quantity");
+                continue;
+            }
+
+            Prescription p = prescriptionManager.createPrescription(medicalName, quantity);
+
+            if(p == null) {
+                System.out.println("Prescription not found");
+                continue;
+            }
+
+            prescriptionIds.add(p.getId());
+        }
+
+        ctx.getManager(AppointmentManager.class)
+            .resolveAppoinment(selected.getId(), service, prescriptionIds, notes);
+    }
+
+    private void handleViewPatientInfo() {
+        Patient p = ctx.getManager(UserManager.class)
+            .getRepository(PatientRepository.class)
+            .get(selected.getPatientId());
+
+        String patientInfo = String.format(
+            "Name: %s\nDOB: %s\nGender: %s\nContact Info: %s",
+            p.name, p.dob, p.isMale ? "Male" : "Female", p.contact
+        );
+
+        SimpleMenu patientInfoMenu = new SimpleMenu(patientInfo, null);
+
+        menuNav.addMenu(patientInfoMenu);
+    }
+
     private void onAppointmentSelect(Appointment appointment){
         this.selected = appointment;
 
@@ -131,7 +191,29 @@ public class ViewAppointmentsService implements IService {
     }
 
     private AbstractMenu getAppointmentSelectDoctorMenu(Appointment appointment) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Patient p = ctx.getManager(UserManager.class)
+            .getRepository(PatientRepository.class)
+            .get(appointment.getPatientId());
+
+        String appointmentInfo = String.format(
+            "Appointment with %s on %s\nStatus: %s",
+            p.name, appointment.getDate(),
+            appointment.getStatus().toString()
+        );
+
+        SimpleMenu appointmentMenu = new SimpleMenu(appointmentInfo, null);
+
+        if(appointment.getStatus() == AppointmentStatus.PENDING) {
+            appointmentMenu.addOption(viewPatientInfoOption);
+            appointmentMenu.addOption(resolveAppointmentOption);
+        }
+
+        if(appointment.getStatus() == AppointmentStatus.ACCEPTED) {
+            appointmentMenu.addOption(viewPatientInfoOption);
+            appointmentMenu.addOption(completeAppointmentOption);
+        }
+
+        return appointmentMenu;
     }
 
     private AbstractMenu getAppointmentSelectAdminMenu(Appointment appointment) {
