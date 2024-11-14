3687edf4043f8b88ff6b4795a8994918d986a8f6
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-14 12:17:29 +0800

    Override super class hasOption(key) to detect item selection

--- src/hms/ui/PaginatedListSelector.java
+++ src/hms/ui/PaginatedListSelector.java
@@ -12,18 +12,32 @@ public class PaginatedListSelector<T> extends PaginatedListViewer<T>{
     }
 
     @Override
-    public void executeOption(String key){
-        if (hasOption(key)){
-            options.get(key).execute();
+    public boolean hasOption(String key){
+        if (super.hasOption(key)){
+            return true;
         }
 
         try{
             int index = Integer.parseInt(key) - 1;
-            if (0 <= index && index < paginatedList.getLength()){
-                onSelect.accept(paginatedList.getItem(index));
-            }
+            return 0 <= index && index < paginatedList.getLength();
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException("Invalid option");
+            return false;
+        }
+    }
+
+    @Override
+    public void executeOption(String key){
+        if (!hasOption(key))
+            throw new IllegalArgumentException("Invalid option key");
+            
+        try{
+            int index = Integer.parseInt(key) - 1;
+            // Guaranteed that the index is within the bounds
+            // from implementation of hasOption
+            onSelect.accept(paginatedList.getItem(index));
+        } catch (NumberFormatException e){
+            // If the key is not a number, it must be a valid option key
+            options.get(key).execute();
         }
     }
 }
