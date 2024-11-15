59efee288fbb2e55b31d777448c4033d6fd4c6dd
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-16 01:57:41 +0800

    Add create method

--- src/hms/user/repository/DoctorRepository.java
+++ src/hms/user/repository/DoctorRepository.java
@@ -2,10 +2,19 @@ package hms.user.repository;
 
 import hms.manager.ManagerContext;
 import hms.user.model.Doctor;
+import java.util.Date;
 
 public class DoctorRepository extends UserRepository<Doctor>{
     public DoctorRepository(ManagerContext managerContext) {
         super(managerContext);
+    }
+
+    public Doctor createDoctor(String name, boolean isMale, String contact, Date dob) {
+        Doctor d = new Doctor(ctx, name, isMale, contact, dob);
+
+        models.add(d);
+
+        return d;
     }
 
     @Override
