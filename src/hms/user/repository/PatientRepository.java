cc7b94aef4da9863983c868e2121588d3d909653
Neyvivu <vuthaonguyen26052005@gmail.com>
2024-11-16 22:10:05 +0800

    Update Repository package with Javadocs

--- src/hms/user/repository/PatientRepository.java
+++ src/hms/user/repository/PatientRepository.java
@@ -4,11 +4,29 @@ import hms.manager.ManagerContext;
 import hms.user.model.Patient;
 import java.util.Date;
 
+/**
+ * Repository class for managing patient users in the hospital management system.
+ */
 public class PatientRepository extends UserRepository<Patient> {
+
+    /**
+     * Constructs a new PatientRepository with the specified ManagerContext.
+     *
+     * @param managerContext the ManagerContext to be used by the PatientRepository
+     */
     public PatientRepository(ManagerContext managerContext) {
         super(managerContext);
     }
 
+    /**
+     * Creates a new Patient with the specified details.
+     *
+     * @param name the name of the Patient
+     * @param isMale the gender of the Patient
+     * @param contact the contact information of the Patient
+     * @param dob the date of birth of the Patient
+     * @return the created Patient
+     */
     public Patient createPatient(String name, boolean isMale, String contact, Date dob) {
         Patient p = new Patient(ctx, name, isMale, contact, dob);
 
@@ -17,6 +35,11 @@ public class PatientRepository extends UserRepository<Patient> {
         return p;
     }
 
+    /**
+     * Creates an empty Patient model.
+     *
+     * @return a new empty Patient
+     */
     @Override
     public Patient createEmptyModel() {
         return new Patient(this.ctx);
--- src/hms/user/repository/PharmacistRepository.java
+++ src/hms/user/repository/PharmacistRepository.java
@@ -4,11 +4,29 @@ import hms.manager.ManagerContext;
 import hms.user.model.Pharmacist;
 import java.util.Date;
 
+/**
+ * Repository class for managing pharmacist users in the hospital management system.
+ */
 public class PharmacistRepository extends UserRepository<Pharmacist> {
+
+    /**
+     * Constructs a new PharmacistRepository with the specified ManagerContext.
+     *
+     * @param managerContext the ManagerContext to be used by the PharmacistRepository
+     */
     public PharmacistRepository(ManagerContext managerContext) {
         super(managerContext);
     }
 
+    /**
+     * Creates a new Pharmacist with the specified details.
+     *
+     * @param name the name of the Pharmacist
+     * @param isMale the gender of the Pharmacist
+     * @param contact the contact information of the Pharmacist
+     * @param dob the date of birth of the Pharmacist
+     * @return the created Pharmacist
+     */
     public Pharmacist createPharmacist(String name, boolean isMale, String contact, Date dob) {
         Pharmacist p = new Pharmacist(ctx, name, isMale, contact, dob);
 
@@ -17,6 +35,11 @@ public class PharmacistRepository extends UserRepository<Pharmacist> {
         return p;
     }
 
+    /**
+     * Creates an empty Pharmacist model.
+     *
+     * @return a new empty Pharmacist
+     */
     @Override
     public Pharmacist createEmptyModel() {
         return new Pharmacist(super.ctx);
