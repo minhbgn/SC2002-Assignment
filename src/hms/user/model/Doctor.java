7495bd355741ae55622575b12a3e9aba3a523702
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-17 19:17:55 +0800

    chore: Remove debug messages

--- src/hms/user/model/Doctor.java
+++ src/hms/user/model/Doctor.java
@@ -4,7 +4,6 @@ import hms.common.id.IdManager;
 import hms.manager.ManagerContext;
 import hms.utils.Timeslot;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -56,13 +55,9 @@ public class Doctor extends User {
         // Hydrate busyTimeslots
         if (data.get("busyTimeslots").equals("")) return; // No busy timeslots
 
-        System.out.println("busyTimeslots: " + data.get("busyTimeslots"));
-
         String[] busyTimeslotsSerialized = data.get("busyTimeslots").split(DELIMITER);
-        System.out.println("busyTimeslotsSerialized: " + Arrays.toString(busyTimeslotsSerialized));
 
         for (String timeslot : busyTimeslotsSerialized){
-            System.out.println("Adding timeslot: " + timeslot);
             this.busyTimeslots.add(new Timeslot(timeslot));
         }
     }
