44e984baf22771721f0f5623a948a1987211cd71
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-17 17:01:54 +0800

    Add busy timeslots to Doctor

--- src/hms/user/model/Doctor.java
+++ src/hms/user/model/Doctor.java
@@ -2,15 +2,27 @@ package hms.user.model;
 
 import hms.common.id.IdManager;
 import hms.manager.ManagerContext;
+import hms.utils.Timeslot;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class Doctor extends User {
+    private static char DELIMITER = '/';
+
+    private final List<Timeslot> busyTimeslots = new ArrayList<>();
+
 	/**
 	 * Empty constructor to be used for hydration
 	 * @param ctx The manager context to let this User access other classes
 	 */
     public Doctor(ManagerContext ctx) {
         super(ctx);
+    }
+
+    public List<Timeslot> getBusyTimeslots() {
+        return busyTimeslots;
     }
 
     /**
@@ -30,5 +42,31 @@ public class Doctor extends User {
     @Override
     public String toString() {
         return super.toString()+"\n User type: [Doctor]";
+    }
+
+    @Override
+    public void hydrate(HashMap<String, String> data){
+        super.hydrate(data);
+
+        // Hydrate busyTimeslots
+        String[] busyTimeslotsSerialized = data.get("busyTimeslots").split(Character.toString(DELIMITER));
+        for (String timeslot : busyTimeslotsSerialized){
+            this.busyTimeslots.add(new Timeslot(timeslot));
+        }
+    }
+
+    @Override
+    public HashMap<String, String> serialize(){
+        HashMap<String, String> data = super.serialize();
+
+        // Serialize busyTimeslots
+        StringBuilder busyTimeslotsSerialized = new StringBuilder();
+        for (Timeslot timeslot : this.busyTimeslots){
+            busyTimeslotsSerialized.append(timeslot.toString()).append(DELIMITER);
+        }
+
+        data.put("busyTimeslots", busyTimeslotsSerialized.toString());
+
+        return data;
     }
 }
