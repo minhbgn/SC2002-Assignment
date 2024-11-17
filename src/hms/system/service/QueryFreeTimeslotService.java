19a9001b8d47ad6272661cba528cc504926a359c
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-17 19:17:24 +0800

    fix: Round up timeslot boundaries in case doctor busy time does not coincide with intervals between timeslots.

--- src/hms/system/service/QueryFreeTimeslotService.java
+++ src/hms/system/service/QueryFreeTimeslotService.java
@@ -194,13 +194,51 @@ public class QueryFreeTimeslotService implements IService {
         return new Timeslot(dayStart, dayEnd);
     }
 
+    /**
+     * Utility method for calculating the next valid timeslot boundary for the user to select from. </p>
+     * 
+     * A timeslot boundary is a time that the user can select
+     * for the start time or end time of an appointment. 
+     * 
+     * @param current The time to calculate the next valid timeslot boundary from
+     * @return The next valid timeslot boundary.
+     * If the next valid timeslot boundary is after the working hours end,
+     * then the next day's working hours start time is returned.
+     */
+    private Date calculateNextValidTimeslotBoundary(Date current){
+        Calendar calendar = Calendar.getInstance();
+        // Calculate the end of the working hours of the current day
+        calendar.setTime(current);
+        calendar.set(Calendar.HOUR_OF_DAY, WORKING_HOURS_END);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        Date endOfWorkingHours = calendar.getTime();
+
+        // Set the calendar to the start of the working hours
+        // of the current day
+        calendar.set(Calendar.HOUR_OF_DAY, WORKING_HOURS_START);
+
+        // Move the calendar until we find the next valid timeslot boundary
+        while(current.after(calendar.getTime())){
+            calendar.add(Calendar.MINUTE, MINIMUM_APPOINTMENT_DURATION_IN_MINUTES);
+        }
+
+        // If the next valid timeslot boundary is after the working hours end,
+        // then move on to the next day
+        if(calendar.getTime().after(endOfWorkingHours)){
+            calendar.add(Calendar.DATE, 1);
+            calendar.set(Calendar.HOUR_OF_DAY, WORKING_HOURS_START);
+        }
+        
+        return calendar.getTime();
+    }
 
     /** Utility method for calculating the available start times for the user to select from */
     private List<Date> calculateAvailableStartTimes() {
         List<Date> availableStartTimes = new ArrayList<>();
 
         for(Timeslot timeslot : freeTimeslots) {
-            Date startTime = timeslot.getStartTime();
+            Date startTime = calculateNextValidTimeslotBoundary(timeslot.getStartTime());
             // startTime < timeslot.getEndTime()
             // This is to ensure the appointment can be scheduled meaningfully (i.e. duration != 0)
             while (startTime.before(timeslot.getEndTime())) {
@@ -210,7 +248,8 @@ public class QueryFreeTimeslotService implements IService {
                 // The next start time should be at least
                 // MINIMUM_APPOINTMENT_DURATION_IN_MINUTES
                 // after the current start time
-                startTime = new Date(startTime.getTime() + MINIMUM_APPOINTMENT_DURATION_IN_MINUTES * 60 * 1000);
+                startTime = new Date(startTime.getTime()
+                    + MINIMUM_APPOINTMENT_DURATION_IN_MINUTES * 60 * 1000);
             }
         }
 
@@ -222,17 +261,17 @@ public class QueryFreeTimeslotService implements IService {
         List<Date> availableEndTimes = new ArrayList<>();
 
         // The end time should be at least MINIMUM_APPOINTMENT_DURATION_IN_MINUTES after the start time
-        Date appointmentEndTime = new Date(selectedStartTime.getTime()
+        Date endTime = new Date(selectedStartTime.getTime()
             + MINIMUM_APPOINTMENT_DURATION_IN_MINUTES * 60 * 1000);
 
         // endTime <= selectedFreeTimeslot.getEndTime()
         // This is because the appointment the appointment can end
         // right before the next appointment starts
-        while (appointmentEndTime.before(selectedFreeTimeslot.getEndTime()) ||
-            appointmentEndTime.equals(selectedFreeTimeslot.getEndTime())) {
+        while (endTime.before(selectedFreeTimeslot.getEndTime()) ||
+            endTime.equals(selectedFreeTimeslot.getEndTime())) {
             
-            availableEndTimes.add(appointmentEndTime);
-            appointmentEndTime = new Date(appointmentEndTime.getTime()
+            availableEndTimes.add(endTime);
+            endTime = new Date(endTime.getTime()
                 + MINIMUM_APPOINTMENT_DURATION_IN_MINUTES * 60 * 1000);
         }
 
