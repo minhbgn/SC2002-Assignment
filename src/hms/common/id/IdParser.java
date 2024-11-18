eacfc15699fc286601371ab783a9f4a7c26f6b61
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-11-18 21:28:13 +0800

    docs: Add javadoc to Id classes

--- src/hms/common/id/IdParser.java
+++ src/hms/common/id/IdParser.java
@@ -3,9 +3,24 @@ package hms.common.id;
 import hms.common.IModel;
 import java.util.HashMap;
 
+/**
+ * Parser for Ids of registered classes.
+ * This class is used to manage registered classes and their ID prefixes.
+ * <p>
+ * This is to ensure that each class has a unique Id prefix when generating Ids, 
+ * therefore making an id unique across all classes in combination with the Id suffix.
+ * @see IdRegistry IdRegistry for managing the Id suffix. 
+ */
 public class IdParser {
+    /** The map of classes to their ID prefixes. */
     private static final HashMap<Class<? extends IModel>, String> classToIdPrefix = new HashMap<>();
 
+    /**
+     * Adds a class to the registry with the specified prefix.
+     * This method is package-private and should only be called by the IdManager.
+     * @param clazz The class to add to the registry.
+     * @param prefix The prefix to use for the class.
+     */
     static void addClass(Class<? extends IModel> clazz, String prefix) {
         if (clazz == null) {
             throw new IllegalArgumentException("Class cannot be null");
